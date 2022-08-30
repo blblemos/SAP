2ef8328e6e0cadd30cd89c0942253e85fbc96c1c
blblemos <blblemos17@gmail.com>
2022-08-30 16:42:04 -0300

    Refatorando Itens(Correção cadastro)

--- src/Pages/Itens/cadastro.jsx
+++ src/Pages/Itens/cadastro.jsx
@@ -4,14 +4,13 @@ import { useNavigate } from 'react-router-dom';
 import VMasker from "vanilla-masker";
 
 import NavbarMenu from '../../Components/Navbar/Navbar';
-import {api, Config} from '../../Services/api';
+import useApi from '../../Services/useApi';
 import Schema from '../../Utils/ShemaItens';
 
 import '../../Styles/form.css';
 
 function CadastrarItem(){
   const navigateTo = useNavigate();
-  const config = Config();
   const [valorMedio, setValorMedio] = useState();
   function onChange(vmed) {
     const valor_medio = VMasker.toMoney(vmed, {
@@ -22,23 +21,25 @@ function CadastrarItem(){
     });
     setValorMedio(valor_medio);
   }
-  function onSubmit(values) {
-    const bodyParameters = {
-      nome: values.NomeItem,
-      catmat: values.Catmat,
-      valorMed: values.ValorMedio,
-      descricao: values.DescricaoItem,
-    };
-      api.post('itens',bodyParameters, config).then(function () {
-        alert(values.NomeItem+' Cadastrado Com Sucesso!');
+  const [save] = useApi({
+    url: `/itens`,
+    method: 'post',
+    onCompleted: (response) => {
+      if (!response.error){
         navigateTo('/colic/itens');
-      }).catch(function (error) {
-        let msgError = '';
-        for (var index = 0; index < error.response.data.length; index++) {
-          msgError = msgError+error.response.data[index].message+'\n';
-        }
-        alert(msgError);
-      });
+      }
+    }
+  });
+  function onSubmit(values) {
+    var bodyParameters = {
+      nome: values.nome,
+      catmat: values.catmat,
+      valorMed: valorMedio,
+      descricao: values.descricao,
+    };
+    save({
+      data: bodyParameters,
+    });
     
   }
   return (
@@ -49,10 +50,10 @@ function CadastrarItem(){
           validationSchema={Schema}
           onSubmit={onSubmit}
           initialValues={{
-            NomeItem: '',
-            Catmat: '', 
-            ValorMedio: '',
-            DescricaoItem: '',
+            nome: '',
+            catmat: '', 
+            valorMed: '',
+            descricao: '',
           }}
           enableReinitialize
         >
@@ -68,34 +69,34 @@ function CadastrarItem(){
                   <div className="form-elements-column">
                     <label>Nome</label>
                     <Field
-                      className={errors.NomeItem && touched.NomeItem ? "form-input form-input-w100 form-input-error" : "form-input form-input-w100 "} 
+                      className={errors.nome && touched.nome ? "form-input form-input-w100 form-input-error" : "form-input form-input-w100 "} 
                       type="text"
-                      name='NomeItem'
+                      name='nome'
                     />
                     <label>Valor Médio</label>
                     <Field
-                      className={errors.ValorMedio && touched.ValorMedio ? "form-input form-input-w100 form-input-error" : "form-input form-input-w100 "}  
+                      className={errors.valorMed && touched.valorMed ? "form-input form-input-w100 form-input-error" : "form-input form-input-w100 "}  
                       type="text"
-                      name='ValorMedio'
+                      name='valorMed'
                       value={valorMedio}
-                      onBlur={onChange(values.ValorMedio)}
+                      onBlur={onChange(values.valorMed)}
                     />
                   </div>
                   <div className="form-elements-column">
                     <label>CATMAT</label>
                     <Field
-                      className={errors.Catmat && touched.Catmat ? "form-input form-input-w100 form-input-error" : "form-input form-input-w100 "}  
+                      className={errors.catmat && touched.catmat ? "form-input form-input-w100 form-input-error" : "form-input form-input-w100 "}  
                       type="text"
-                      name='Catmat' 
+                      name='catmat' 
                     />
                   </div>
                 </div>
                 <label>Descrição</label>
                     <Field
                       as='textarea'
-                      className={errors.DescricaoItem && touched.DescricaoItem ? "form-input form-input-w100 form-input-error form-input-textarea" : "form-input form-input-textarea form-input-w100 "} 
+                      className={errors.descricao && touched.descricao ? "form-input form-input-w100 form-input-error form-input-textarea" : "form-input form-input-textarea form-input-w100 "} 
                       type="textarea"
-                      name='DescricaoItem'
+                      name='descricao'
                     />
                 <div className="form-footer">
                   <button 
