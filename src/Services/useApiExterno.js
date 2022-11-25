6c5d53a4db760cbb9f721ff0ba38b6f50aca3617
Bruno Lemos <52580590+blblemos@users.noreply.github.com>
2022-11-25 09:18:57 -0300

    Update useApiExterno.js
    Update porta

--- src/Services/useApiExterno.js
+++ src/Services/useApiExterno.js
@@ -18,7 +18,7 @@ export default function useApi(config){
     
     try {
       response = await axios({
-        baseURL: 'http://200.128.8.98:8080',
+        baseURL: 'http://200.128.8.98:6731',
         ...config,
         ...localConfig,
         
